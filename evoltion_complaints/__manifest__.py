8ea18772c9d2cc3671af3a1b589096f0d738b769
Paul Chipana <paulhackingdeveloper@gmail.com>
2025-06-10 15:51:32 -0500

    [UPDATE] css /reclamo styles

---
+++ evoltion_complaints/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Evoltion - personalizaciones al modulo Reclamos - Soy Calidad',
+    'version': '17.0.1.0.0',
+    'depends': ['mgmtsystem_complaints'],
+    'data': [
+        # otros archivos...
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'evoltion_complaints/static/src/css/complaint_custom.css',
+        ],
+    },
+    'installable': True,
+}
